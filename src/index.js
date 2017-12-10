8b610b38ed5cfa8b6c691f52417ab42354c17901
Lucas Silveira <lucas.fernandes.silveira@gmail.com>
2017-12-10 15:50:39 -0200

    Airbnb linter + Absolute paths config + Reactotron log

---
+++ src/index.js
@@ -0,0 +1,13 @@
+/* Core */
+import 'config/ReactotronConfig';
+import React from 'react';
+
+/* Presentational */
+import { View } from 'react-native';
+
+// import styles from './styles';
+const App = () => (
+  <View />
+);
+
+export default App;
